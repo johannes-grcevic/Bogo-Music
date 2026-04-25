23127a5e80d2c843ecdfa114ffa92f1cc32576ef
Johannes Grcevic <20111621@setu.ie>
2026-04-25 17:33:05 +0100

    Rename FXML file and update stage title to "Bogo Music"

--- src/main/java/ie/setu/bogomusic/main/MusicApplication.java
+++ src/main/java/ie/setu/bogomusic/main/MusicApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class MusicApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MusicApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MusicApplication.class.getResource("music-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        stage.setTitle("Bogo Music");
         stage.setScene(scene);
         stage.show();
     }
